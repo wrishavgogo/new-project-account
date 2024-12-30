ab5052d2dfe99ae3fe204b233d9dd7aee168124d
Wrishav_OSS <wbhattacharyya@gmail.com>
2024-12-30 13:15:59 +0530

    new commit

--- src/main/java/com/Java/Main.java
+++ src/main/java/com/Java/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        System.out.println("we are now able to upload from work laptop as well");
     }
 }
